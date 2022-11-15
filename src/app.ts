8c1de237f96592ffcbdff5a6714c1715092de52b
Roman <fantarose51@gmail.com>
2022-11-15 17:37:25 +0200

    fix: json bodyParser error

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ require('dotenv').config()
 import 'reflect-metadata';
 import express from 'express';
 import config from 'config';
+import bodyParser from 'body-parser';
 import { DbHelper } from './helpers/db.helper';
 import { rootRouter } from './routers/root.router';
 import { NothingType } from './types/nothing.type';
@@ -10,14 +11,14 @@ import { NothingType } from './types/nothing.type';
 const port: string = config.get('port')
 const app = express()
 
+app.use(bodyParser.json())
 app.use(rootRouter)
-app.use(express.json())
 
 DbHelper.initialize()
   .then<NothingType>((): NothingType => {
     app.listen(port, () => { console.log(`Server is running on http://localhost:${port}`) })
     return
-  }).catch<never>((error: any): never => {
+  }).catch<never>((error): never => {
     console.error(error)
     process.exit(1)
 })
