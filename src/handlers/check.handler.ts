107af3e650540d4388d0aeb05221b40ce643474e
Roman <fantarose51@gmail.com>
2022-11-15 17:45:19 +0200

    feat: add logic to RegistrationHandler

--- src/handlers/check.handler.ts
+++ src/handlers/check.handler.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 import { HttpStatusEnum } from '../enums/http-status.enum';
+import {UserLoader} from '../loaders/user.loader';
 
 export class CheckHandler {
-  public static handle(req: Request, res: Response) {
+  public static async handle(req: Request, res: Response) {
     console.log('CheckHandler')
+    const dbUser = await UserLoader.getUser()
+    console.log(dbUser)
     return res.sendStatus(HttpStatusEnum.OK)
   }
 }
--- src/handlers/registration.handler.ts
+++ src/handlers/registration.handler.ts
@@ -1,3 +1,26 @@
-export class SignupHandler {
-  public static handle() {}
+import { Request, Response } from 'express';
+import { HttpStatusEnum } from '../enums/http-status.enum';
+import { DataHelper } from '../helpers/data.helper';
+import { PasswordHelper } from '../helpers/password.helper';
+import { UserIDTypeHelper } from '../helpers/user-id-type.helper';
+import { UserLoader } from '../loaders/user.loader';
+import { UserModel } from '../models/entities/user.model';
+import { ErrorModel } from '../models/error.model';
+
+export class RegistrationHandler {
+  public static async handle(req: Request, res: Response) {
+    console.log('RegistrationHandler')
+    console.log(req.body)
+
+    const result = await UserLoader.saveUser(new UserModel({
+      id: DataHelper.createUUID(),
+      userID: req.body.id,
+      userIDType: UserIDTypeHelper.check(req.body.id),
+      password: await PasswordHelper.hash(req.body.password)
+    }))
+
+    //TODO ??: Add JWT create method
+    // Returns JSON with Status and JWT?
+    return res.sendStatus(HttpStatusEnum.Created)
+  }
 }
